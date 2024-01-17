39a9b901b529fbfe57f3db3c78a64c7d9d84fe07
Azizbek1 <azizochilov569@gmail.com>
2024-01-17 11:17:02 +0500

    ima

--- src/pages/AsosiySahifaPage/AsosiySahifaPage.jsx
+++ src/pages/AsosiySahifaPage/AsosiySahifaPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import ImgAsosiy from './Group 72.jpg'
+import "./style.scss"
 export default function AsosiySahifaPage() {
   return (
-    <div>AsosiySahifaPage</div>
+    <div className="asosiysahifa">
+      <div className='asosiysahifa_title'>
+        «IT sohasi bu – bugun va kelajak. Uni yaxshi o‘rgangan odam juda yaxshi yashaydi» - Komil Allamjonov
+      </div>
+      <div className='asosiysahifa_img'>
+        <img src={ImgAsosiy} alt="jpg" />
+      </div>
+    </div>
   )
 }
